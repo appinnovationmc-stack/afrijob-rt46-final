13df47720c52ba9e5c4e3fd0c3cde8bb137a4488
appinnovationmc-stack <appinnovationmc@gmail.com>
2026-08-24 19:09:42 +0200

    fix: neutralize stale inline ESLint directives

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,10 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 export default [
   {
     ignores: ['dist/**', 'android/**', 'ios/**', 'node_modules/**', 'coverage/**'],
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: 'off',
+    },
   },
   {
     files: ['**/*.{ts,tsx}'],
